ed29bbc37da06f53a24c89b5165c67a16c69a528
Maria Sukhova <mariesukhova@gmail.com>
2023-05-23 22:32:32 +0300

    [step-4] Add new routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,5 +2,6 @@ const apiPath = '/api/v1';
 
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
-  usersPath: () => [apiPath, 'data'].join('/'),
+  dataPath: () => [apiPath, 'data'].join('/'),
+  signupPath: () => [apiPath, 'signup'].join('/'),
 };
